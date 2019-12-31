ff5b9bbe51b7a06973a1bfd29e33ca2c206197dc
Nicolás Quiroz <nh.quiroz@gmail.com>
2019-12-31 17:30:46 -0300

    update Navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,9 @@
+<div class="bg-white-us fixed top-0 left-0 shadow-md p-2 w-full sm:text-right text-center sm:pr-3" style="height: 40px">
+  <a href="https://undefinedschool.io" target="_blank" rel="noopener" class="text-s">
+    <p class="font-montserrat font-medium">
+      <span class="text-black-us">undefined</span>
+      <span class="text-gray-600">sch001</span>
+      <span class="blink text-cyan-us -ml-1">_</span>
+    </p>
+  </a>
+</div>
