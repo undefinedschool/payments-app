c48f622ba8b01fc590a9d0b45f1e468228f2e9ec
nhsz <nh.quiroz@gmail.com>
2020-04-13 06:54:59 -0300

    chore: add money face emoji

--- src/components/Payments/SelectPaymentMethodTitle.svelte
+++ src/components/Payments/SelectPaymentMethodTitle.svelte
@@ -1,4 +1,10 @@
 <h1 class="leading-tight sm:text-3xl text-4xl sm:mb-6 mb-12 text-white-us font-raleway text-center sm:text-left">
   Realizar
-  <span class="font-semibold text-cyan-us">pago</span>
+  <span class="font-semibold text-cyan-us">
+    pago
+    <img
+      class="inline h-auto emoji"
+      src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/twitter/236/money-mouth-face_1f911.png"
+      alt="money mouth face emoji" />
+  </span>
 </h1>
